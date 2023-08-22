40f869500a409479e07a208c62229395ac1e8a80
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-08-22 13:25:38 -0600

    commit

--- portafolio/components/Header.tsx
+++ portafolio/components/Header.tsx
@@ -1,20 +1,42 @@
-import React from 'react'
-import Image from 'next/image'
-import Logo from '../public/img/logo2.jpg'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import Image from "next/image";
+import Logo from "../public/img/logo2.jpg";
+import styles from "../styles/Home.module.css";
+import { Button } from 'react-bootstrap';
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <nav className={styles.nav}>
-        <button
-        type='button'>
-            <Image src={Logo} alt="Imagen Portafolio" width={60} height={60}/>
-
-        </button>
-
-        <button type='button'>Free Consultation</button>
-    </nav>
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          src=""
+          alt="/"
+          width="125"
+          height="50"
+        />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Projects</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Contact</li>
+            </Link>
+          </ul>
+          </div>
+        </div>
+      </div>
   );
 };
 
-export default Header
+export default Header;
